5163a0568c50bd5009e6ec746940451aada90bd7
SamyBK07 <kpeigounousamiou@gmail.com>
2026-03-17 22:05:09 +0100

    Update tts.py

--- tts.py
+++ tts.py
@@ -1,1 +1,14 @@
+from TTS.api import TTS
 
+tts = TTS(model_name="tts_models/fr/css10/vits")
+
+def generate_tts(text):
+
+    output = "response.wav"
+
+    tts.tts_to_file(
+        text=text,
+        file_path=output
+    )
+
+    return output
