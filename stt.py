a4a78096b5349d4176fc1913b40ee666f309f944
SamyBK07 <kpeigounousamiou@gmail.com>
2026-03-17 22:04:16 +0100

    Update stt.py

--- stt.py
+++ stt.py
@@ -1,1 +1,28 @@
+from vosk import Model, KaldiRecognizer
+import wave
+import json
 
+model = Model("model/vosk-model-small-fr-0.22")
+
+def transcribe(audio_path):
+
+    wf = wave.open(audio_path, "rb")
+
+    recognizer = KaldiRecognizer(model, wf.getframerate())
+
+    text = ""
+
+    while True:
+
+        data = wf.readframes(4000)
+
+        if len(data) == 0:
+            break
+
+        if recognizer.AcceptWaveform(data):
+
+            result = json.loads(recognizer.Result())
+
+            text += result.get("text","") + " "
+
+    return text.strip()
